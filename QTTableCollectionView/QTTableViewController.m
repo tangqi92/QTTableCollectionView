44dd5395e8cccbb6745ee2c57286c7ccff6dc7f3
tangqi92 <imtangqi@gmail.com>
2017-02-20 13:08:18 +0800

    Added the 'contentOffsetDictionary' for each type.

--- QTTableCollectionView/QTTableViewController.m
+++ QTTableCollectionView/QTTableViewController.m
@@ -21,7 +21,9 @@ static NSInteger const kNumberOfItemsInSection = 10;
 @interface QTTableViewController ()
 
 // 用于记录 CollectionView 内容的偏移量。
-@property (nonatomic, strong) NSMutableDictionary *contentOffsetDictionary;
+@property (nonatomic, strong) NSMutableDictionary *contentOffsetDictionaryOfExplores;
+@property (nonatomic, strong) NSMutableDictionary *contentOffsetDictionaryOfBoards;
+@property (nonatomic, strong) NSMutableDictionary *contentOffsetDictionaryOfUsers;
 // DataSource / 数据源。
 @property (nonatomic, strong) NSMutableArray *dataSourceExplores;
 @property (nonatomic, strong) NSMutableArray *dataSourceBoards;
@@ -40,7 +42,9 @@ static NSInteger const kNumberOfItemsInSection = 10;
     self.dataSourceExplores = @[].mutableCopy;
     self.dataSourceBoards = @[].mutableCopy;
     self.dataSourceUsers = @[].mutableCopy;
-    self.contentOffsetDictionary = [NSMutableDictionary dictionary];
+    self.contentOffsetDictionaryOfExplores = [NSMutableDictionary dictionary];
+    self.contentOffsetDictionaryOfBoards = [NSMutableDictionary dictionary];
+    self.contentOffsetDictionaryOfUsers = [NSMutableDictionary dictionary];
 
     self.tableView.showsVerticalScrollIndicator = NO;
     self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
@@ -78,22 +82,29 @@ static NSInteger const kNumberOfItemsInSection = 10;
 }
 
 - (void)tableView:(UITableView *)tableView willDisplayCell:(QTTableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath {
-    // 为 CollectionView 设置不同的 collectionViewCellType 用以区别。
+    // 为 TableViewCell 中的 CollectionView 设置不同的 collectionViewCellType 用以区别，此处一共三中样式。
     if (indexPath.section == 0) {
         cell.collectionView.collectionViewCellType = CellTypeExplores;
+        NSInteger index = cell.collectionView.indexPath.row;
+        CGFloat horizontalOffset = [self.contentOffsetDictionaryOfExplores[[@(index) stringValue]] floatValue];
+        // 设置 CollectionView 的 ContentOffset，在'- (void)scrollViewDidScroll:(UIScrollView *)scrollView;' 中存储的。
+        [cell.collectionView setContentOffset:CGPointMake(horizontalOffset, 0)];
     } else if (indexPath.section == 1) {
         cell.collectionView.collectionViewCellType = CellTypeBoards;
+        NSInteger index = cell.collectionView.indexPath.row;
+        CGFloat horizontalOffset = [self.contentOffsetDictionaryOfBoards[[@(index) stringValue]] floatValue];
+        // 设置 CollectionView 的 ContentOffset。
+        [cell.collectionView setContentOffset:CGPointMake(horizontalOffset, 0)];
     } else if (indexPath.section == 2) {
         cell.collectionView.collectionViewCellType = CellTypeUsers;
+        NSInteger index = cell.collectionView.indexPath.row;
+        CGFloat horizontalOffset = [self.contentOffsetDictionaryOfUsers[[@(index) stringValue]] floatValue];
+        // 设置 CollectionView 的 ContentOffset。
+        [cell.collectionView setContentOffset:CGPointMake(horizontalOffset, 0)];
     }
 
     // 设置 CollectionView 的 DataSource 与 Delegate 及所处的 indexPath。
     [cell setCollectionViewDataSourceDelegate:self indexPath:indexPath];
-    NSInteger index = cell.collectionView.indexPath.row;
-
-    CGFloat horizontalOffset = [self.contentOffsetDictionary[[@(index) stringValue]] floatValue];
-    // 设置 CollectionView 的 ContentOffset。
-    [cell.collectionView setContentOffset:CGPointMake(horizontalOffset, 0)];
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
@@ -108,6 +119,7 @@ static NSInteger const kNumberOfItemsInSection = 10;
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
     if (indexPath.section == 0) {
+        // 此处，TableViewCell（UICollectionViewCell）的高度固定。
         return 132;
     } else if (indexPath.section == 1) {
         return 190;
@@ -124,15 +136,15 @@ static NSInteger const kNumberOfItemsInSection = 10;
         case CellTypeExplores:
             return kNumberOfItemsInSection;
             // TODO: 返回真实的数据。
-//            return self.dataSourceExplores.count;
+            //            return self.dataSourceExplores.count;
             break;
         case CellTypeBoards:
             return kNumberOfItemsInSection;
-//            return self.dataSourceBoards.count;
+            //            return self.dataSourceBoards.count;
             break;
         case CellTypeUsers:
             return kNumberOfItemsInSection;
-//            return self.dataSourceUsers.count;
+            //            return self.dataSourceUsers.count;
             break;
         default:
             break;
@@ -148,27 +160,27 @@ static NSInteger const kNumberOfItemsInSection = 10;
             QTExploresCollectionViewCell *cellExplore = (QTExploresCollectionViewCell *) cell;
             [cellExplore setupModel];
             // TODO: 获取到相应的 Model 后进行赋值操作
-//            if (self.dataSourceExplores.count > 0) {
-//                cellExplore.exploreModel = self.dataSourceExplores[indexPath.row];
-//            }
+            //            if (self.dataSourceExplores.count > 0) {
+            //                cellExplore.exploreModel = self.dataSourceExplores[indexPath.row];
+            //            }
         }
     } else if (collectionView.collectionViewCellType == CellTypeBoards) {
         cell = [collectionView dequeueReusableCellWithReuseIdentifier:BoardCollectionViewCellID forIndexPath:indexPath];
         if ([cell isKindOfClass:[QTBoardsCollectionViewCell class]]) {
             QTBoardsCollectionViewCell *cellBoard = (QTBoardsCollectionViewCell *) cell;
             [cellBoard setupModel];
-//            if (self.dataSourceBoards.count > 0) {
-//                cellBoard.boardModel = self.dataSourceBoards[indexPath.row];
-//            }
+            //            if (self.dataSourceBoards.count > 0) {
+            //                cellBoard.boardModel = self.dataSourceBoards[indexPath.row];
+            //            }
         }
     } else if (collectionView.collectionViewCellType == CellTypeUsers) {
         cell = [collectionView dequeueReusableCellWithReuseIdentifier:UserCollectionViewCellID forIndexPath:indexPath];
         if ([cell isKindOfClass:[QTUsersCollectionViewCell class]]) {
             QTUsersCollectionViewCell *cellUser = (QTUsersCollectionViewCell *) cell;
             [cellUser setupModel];
-//            if (self.dataSourceUsers.count > 0) {
-//                cellUser.userModel = self.dataSourceUsers[indexPath.row];
-//            }
+            //            if (self.dataSourceUsers.count > 0) {
+            //                cellUser.userModel = self.dataSourceUsers[indexPath.row];
+            //            }
         }
     }
     return cell;
@@ -178,18 +190,18 @@ static NSInteger const kNumberOfItemsInSection = 10;
     NSInteger position = indexPath.row;
     switch (collectionView.collectionViewCellType) {
         case CellTypeExplores: {
-            // 获取相应的 Moddel。
-//            QTExploreModel *exploreModel = self.dataSourceExplores[indexPath.row];
+            // 获取相应的 Moddel，可进行相应的点击后操作。
+            //            QTExploreModel *exploreModel = self.dataSourceExplores[indexPath.row];
             [SVProgressHUD showSuccessWithStatus:[NSString stringWithFormat:@"Selected CellTypeExplores Item: %ld", position]];
             break;
         }
         case CellTypeBoards: {
-//            QTBoardModel *boardModel = self.dataSourceBoards[indexPath.row];
+            //            QTBoardModel *boardModel = self.dataSourceBoards[indexPath.row];
             [SVProgressHUD showSuccessWithStatus:[NSString stringWithFormat:@"Selected CellTypeBoards Item: %ld", position]];
             break;
         }
         case CellTypeUsers: {
-//            QTUserModel *userModel = self.dataSourceUsers[indexPath.row];
+            //            QTUserModel *userModel = self.dataSourceUsers[indexPath.row];
             [SVProgressHUD showSuccessWithStatus:[NSString stringWithFormat:@"Selected CellTypeUsers Item: %ld", position]];
             break;
         }
@@ -222,7 +234,14 @@ static NSInteger const kNumberOfItemsInSection = 10;
     CGFloat horizontalOffset = scrollView.contentOffset.x;
     QTCollectionView *collectionView = (QTCollectionView *) scrollView;
     NSInteger index = collectionView.indexPath.row;
-    self.contentOffsetDictionary[[@(index) stringValue]] = @(horizontalOffset);
+    // 根据 collectionViewCellType 存储对应的偏移量
+    if (collectionView.collectionViewCellType == CellTypeExplores) {
+        self.contentOffsetDictionaryOfExplores[[@(index) stringValue]] = @(horizontalOffset);
+    } else if (collectionView.collectionViewCellType == CellTypeBoards) {
+        self.contentOffsetDictionaryOfBoards[[@(index) stringValue]] = @(horizontalOffset);
+    } else if (collectionView.collectionViewCellType == CellTypeUsers) {
+        self.contentOffsetDictionaryOfUsers[[@(index) stringValue]] = @(horizontalOffset);
+    }
 }
 
 // 该方法仅用于测试：模拟刷新操作。
